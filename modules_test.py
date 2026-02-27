b2542f7495a4c38bdf118c3831af61bb5ad7a7d6
jundaBu <juandacarros@gmail.com>
2026-02-27 17:56:44 +0000

    Added code for the Posts section

--- modules_test.py
+++ modules_test.py
@@ -15,9 +15,32 @@ from modules import display_post, display_activity_summary, display_genai_advice
 class TestDisplayPost(unittest.TestCase):
     """Tests the display_post function."""
 
-    def test_foo(self):
-        """Tests foo."""
-        pass
+    def test_post_rendering(self):
+        """Tests that the post renders with the correct information."""
+        at = AppTest.from_function(
+            display_post,
+            args=(
+                "testuser",
+                "http://example.com/user.jpg",
+                "2024-01-01 12:00:00",
+                "This is a test post.",
+                "http://example.com/post.jpg",
+            ),
+        )
+        at.run()
+
+        # Check if the html component is rendered
+        self.assertEqual(len(at.html), 1)
+        html_output = at.html[0].value
+
+        # Check for key pieces of information in the rendered HTML
+        self.assertIn("testuser", html_output)
+        self.assertIn("http://example.com/user.jpg", html_output)
+        self.assertIn("2024-01-01 12:00:00", html_output)
+        self.assertIn("This is a test post.", html_output)
+        self.assertIn("http://example.com/post.jpg", html_output)
+        self.assertIn("Likes", html_output)
+        self.assertIn("Comments", html_output)
 
 
 class TestDisplayActivitySummary(unittest.TestCase):
